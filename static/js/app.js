c7446c323556e25ad78cea3bcd00a0e335597b48
Kobert Lam <kobertlam@gmail.com>
2022-01-02 14:55:20 -0500

    completed section 11.5.4

--- static/js/app.js
+++ static/js/app.js
@@ -25,4 +25,30 @@ function buildTable(data){
             cell.text(val);
         });
     });
-}
+};
+
+function handleClick(){
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    // declare a dafault filter variable
+    let filteredData = tableData;
+
+    // Check if a date is entered and filter the data using that date
+    if (date) {
+        // Apply 'filter' to the table data to only keep the
+        // rows where the 'datetime' value matched the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for the form button with HTML tag id 'filter-btn'
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
+
